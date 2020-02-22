4e33fabc9df6c7e7845ebbe6880875d7b0f34d83
Akhayika <akhayika@gmail.com>
2020-02-22 15:45:11 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import LineChart from './linechart.jsx';
 
 const columns=[
   ["My Number",30,200,100,400,150,250],
@@ -18,10 +19,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <LineChart/>
       </div>
     );
   }
